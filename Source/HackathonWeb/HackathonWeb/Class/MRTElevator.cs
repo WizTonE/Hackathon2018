2307e494680ab41c6d4f1e9d8ce8601b3cbb643b
Unknown <wiztone@gmail.com>
2018-08-18 15:23:46 +0800

    Add MRT Elevators data

---
+++ Source/HackathonWeb/HackathonWeb/Class/MRTElevator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace HackathonWeb.Class
+{
+    public class MRTElevator
+    {
+        public MRTElevator()
+        {
+        }
+        public string Line
+        {
+            get;
+            set;
+        }
+        public string Station
+        {
+            get;
+            set;
+        }
+        public string Location
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Source/HackathonWeb/HackathonWeb/Class/OpenDataStorage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+namespace HackathonWeb.Class
+{
+    public class OpenDataStorage
+    {
+        private static OpenDataStorage _instance;
+        public static OpenDataStorage Instance => _instance ?? (_instance = new OpenDataStorage());
+
+        private List<MRTElevator> _mRTElevators;
+        public List<MRTElevator> MRTElevators => _mRTElevators ?? (
+            _mRTElevators = File.ReadAllLines("OpenDataSource/MRTElevators.csv").Select(item =>
+            {
+                var data = item.Split(',');
+                return new MRTElevator { Line = data[0], Station = data[1], Location = data[2] };
+            }).ToList());
+
+        private OpenDataStorage()
+        {
+        }
+    }
+}
