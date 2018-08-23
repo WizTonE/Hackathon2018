d2bb52cb5ad2ad2561307f2ac62dd0c253906e50
lancetchang <lancetchang@gmail.com>
2018-08-23 12:45:56 +0800

    test for map marker

---
+++ Source/HackathonWeb/HackathonWeb/Controllers/PocRestaurantMapController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HackathonWeb.Models;
+
+namespace HackathonWeb.Controllers
+{
+    public class PocRestaurantMapController : Controller
+    {
+        // GET: Poc
+        public ActionResult Index()
+        {
+            List<Restaurant> restaurantLocation = new List<Restaurant>();
+            //Restaurant data = new Restaurant();
+            //data.Address = "台北市中山區中山北路二段36巷18號";
+            //restaurantLocation.Add(data);
+            foreach (Restaurant data in OpenDataStorage.Instance.Restaurants)
+            {
+                restaurantLocation.Add(data);
+            }
+
+            return View(restaurantLocation.ToList());
+        }
+    }
+}
