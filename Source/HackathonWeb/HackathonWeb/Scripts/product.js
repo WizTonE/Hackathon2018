b9ee35a8574e351cebfe9902c971681f631d5c1c
Robert Shiue <robertdx@gmail.com>
2018-08-24 14:34:15 +0800

    styles updates to map and thumbs and more.

--- Source/HackathonWeb/HackathonWeb/Scripts/product.js
+++ Source/HackathonWeb/HackathonWeb/Scripts/product.js
@@ -269,7 +269,7 @@ const MetroNavView = Vue.component('metro-nav-view',
         },
         template:
             '<div class="setup-container">\
-                <div >\
+                <div class="map-view metro-pic-box">\
                   <img src="../Scripts/assets/mrt-Zhongshan.png" width="100%" height="100%"> \
                   1.出口電梯：<br>出口4（南京西路北側之淡水線線形公園內）<br>出口5（南京西路與赤峰街交叉東北隅）<br>出口6（南京西路與赤峰街交叉東南隅）<br>2.月臺電梯：<br>淡水信義線：大廳層中央<br>松山新店線：大廳層東側<br>\
                 </div>\
@@ -293,8 +293,8 @@ const UberNavView = Vue.component('uber-nav-view',
         },
         template:
             '<div class="setup-container">\
-                <div>\
-                    <div class="setup-container"><div><iframe width="100%" height="1500" src="https://m.uber.com/?client_id=2dv2-1SM7rwg9_ogbq3Sxe4BYuNQrDxi&action=setPickup&pickup[latitude]=25.077883&pickup[longitude]=121.5727394&pickup[nickname]=CurrentPlace&dropoff[latitude]=25.0596028&dropoff[longitude]=121.5602683&dropoff[nickname]=Home" frameborder="0" allowfullscreen></iframe></div></div>\
+                <div class="map-view">\
+                    <div><div><iframe width="100%" height="1500" src="https://m.uber.com/?client_id=2dv2-1SM7rwg9_ogbq3Sxe4BYuNQrDxi&action=setPickup&pickup[latitude]=25.077883&pickup[longitude]=121.5727394&pickup[nickname]=CurrentPlace&dropoff[latitude]=25.0596028&dropoff[longitude]=121.5602683&dropoff[nickname]=Home" frameborder="0" allowfullscreen></iframe></div></div>\
                 </div>\
                 <way-nav-view current="2"></way-nav-view>\
             </div>'
@@ -316,7 +316,7 @@ const DestinationView = Vue.component('destination-view',
         },
         template:
             '<div class="setup-container">\
-                <div>destination view</div>\
+                <div class="map-view">destination view</div>\
                 <way-nav-view current="3"></way-nav-view>\
             </div>'
     });
@@ -336,7 +336,7 @@ const FeedbackView = Vue.component('feedback-view',
             }
         },
         template:
-            '<div class="setup-container"><div>feedback view</div></div>'
+            '<div class="setup-container"><div class="map-view">feedback view</div></div>'
     });
 
 /**
